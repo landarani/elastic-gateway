09240c8efbbf40688e5f2212ddc5400d9d7daabe
Shahram Landarani <shahram.landarani@resmed.com.au>
2019-09-14 10:19:11 +1000

    initial project strcuture

---
+++ src/main/java/resmed/hi/demo/ForwardBasicAuthenticationGatewayFilterFactory.java
@@ -0,0 +1,28 @@
+package resmed.hi.demo;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+public class ForwardBasicAuthenticationGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return (exchange, chain) -> {
+            String token = "Basic " + Base64.getEncoder().encodeToString((config.getName() + ":" + config.getValue()).getBytes());
+            ServerHttpRequest request = exchange.getRequest().mutate()
+                .headers(httpHeaders -> httpHeaders.set(AUTHORIZATION, token))
+                .build();
+
+            return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+}
